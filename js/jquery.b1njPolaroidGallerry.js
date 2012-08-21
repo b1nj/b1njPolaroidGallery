93b637a38eff23b481ebd051460cd25ba6137321
b1nj <b.simon@pixellweb.com>
2012-08-21 16:42:27 -0400

    do private the function shiftAfeterRotate and randomXToY

--- js/jquery.b1njPolaroidGallerry.js
+++ js/jquery.b1njPolaroidGallerry.js
@@ -4,7 +4,7 @@
 * Copyright (c) 2012 b1nj Licensed MIT */
 (function ($)
 {
-    jQuery.fn.b1njPolaroidGallery = function (options)
+    $.fn.b1njPolaroidGallery = function (options)
     {
         var settings = $.extend( {
             'maxRotation' : 20,
@@ -95,9 +95,34 @@
                 });
             });
         });
+        
+        // Private functions
+        
+        // Function to get random number upto m
+        // http://roshanbh.com.np/2008/09/get-random-number-range-two-numbers-javascript.html
+        function randomXToY(minVal,maxVal,floatVal)
+        {
+            var randVal = minVal+(Math.random()*(maxVal-minVal));
+            return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
+        }
+        
+        // Fuction get the shift after rotate the photo
+        // http://www.maths-forum.com/trigonometrie-rotation-d-un-rectangle-129467.php
+        // //x = (1/V2).V(CD² + AD²) * V(1-cos(alpha)) * sin[(180° - alpha)/2 - arctg(AD/CD)]
+        function shiftAfeterRotate(height, width, rotate)
+        {
+            if ($.browser.msie  && parseInt($.browser.version, 10) === 8) {
+                return 0;
+            }
+            var x = (1/Math.sqrt(2)) * 
+            Math.sqrt(Math.pow(width,2) + Math.pow(height,2)) * 
+            Math.sqrt(1 - Math.cos(rotate * (Math.PI / 180))) * 
+            Math.sin(((180 - rotate)/2 - (Math.atan2(height, width)  * (180 / Math.PI))) * (Math.PI / 180));
+            return x;
+        }
         return this;
     };
-    jQuery.fn.b1njPolaroidGalleryRotate = function (rotation)
+    $.fn.b1njPolaroidGalleryRotate = function (rotation)
     {
         if ($.browser.msie  && parseInt($.browser.version, 10) === 8) {
             if (rotation >= 0) {
@@ -121,26 +146,3 @@
         return this;
     }
 }) (jQuery);
-
-// Function to get random number upto m
-// http://roshanbh.com.np/2008/09/get-random-number-range-two-numbers-javascript.html
-function randomXToY(minVal,maxVal,floatVal)
-{
-    var randVal = minVal+(Math.random()*(maxVal-minVal));
-    return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
-}
-
-// Fuction get the shift after rotate the photo
-// http://www.maths-forum.com/trigonometrie-rotation-d-un-rectangle-129467.php
-// //x = (1/V2).V(CD² + AD²) * V(1-cos(alpha)) * sin[(180° - alpha)/2 - arctg(AD/CD)]
-function shiftAfeterRotate(height, width, rotate)
-{
-    if ($.browser.msie  && parseInt($.browser.version, 10) === 8) {
-        return 0;
-    }
-    var x = (1/Math.sqrt(2)) * 
-    Math.sqrt(Math.pow(width,2) + Math.pow(height,2)) * 
-    Math.sqrt(1 - Math.cos(rotate * (Math.PI / 180))) * 
-    Math.sin(((180 - rotate)/2 - (Math.atan2(height, width)  * (180 / Math.PI))) * (Math.PI / 180));
-    return x;
-}
